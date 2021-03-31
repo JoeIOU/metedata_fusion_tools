b825779f94058b4ab8f97b490b9cc8cfe7f6efdb
JoeIOU <78242596+JoeIOU@users.noreply.github.com>
2021-03-31 11:44:27 +0800

    Update neo4j_service.py

--- src/service/neo4j_service.py
+++ src/service/neo4j_service.py
@@ -3,12 +3,25 @@ from json import dumps
 from flask import Response, request
 from db.neo4j_conn import neo4j_graph as graph
 from config.config import cfg as config
+from service import utils_serv as utl
+from flask_httpauth import HTTPBasicAuth
+from common import authorization as au
 from httpserver import httpserver
 
 logger = config.logger
 app = httpserver.getApp()
+auth = HTTPBasicAuth()
 
 domain_root = '/md/graph'
+
+
+# 验证token
+@auth.verify_password
+def verify_password(username, password):
+    bool = au.verify_password(username, password)
+    # if bool:
+    #     utl.get_login_user_privilege()
+    return bool
 
 
 def serialize_model(model):
@@ -17,11 +30,11 @@ def serialize_model(model):
     if release is not None:
         s_ver = str(release)
     return {
-        'id': model['id'],
-        'code': model['entity_code'],
+        'entity_id': model['entity_id'],
+        'entity_code': model['entity_code'],
         'title': model['name'],
-        'name': model['name'],
-        'released': s_ver
+        'entity_name': model['entity_name'],
+        'schema': model['schema']
     }
 
 
@@ -40,14 +53,20 @@ def serialize_model_path(model):
 
 def serialize_cast(cast):
     return {
-        'REL_TABLE': cast[0],
-        'REL_TYPE': cast[1],
-        'REL_PK': cast[2],
-        'REL_FK': cast[3]
+        'name': cast[0],
+        'label': cast[1],
+        'relation_id': cast[2],
+        'relation_type': cast[3],
+        'relation_desc': cast[4],
+        'from_entity_name': cast[5],
+        'from_fields_name': cast[6],
+        'to_entity_name': cast[7],
+        'to_fields_name': cast[8]
     }
 
 
 @app.route(domain_root + "/graph/<title>/<flag>")
+@auth.login_required
 def get_graph(title, flag):
     if title is None or title == '':
         return None
@@ -72,14 +91,19 @@ def query_relation_graph(title, flag):
 
 
 @app.route(domain_root + "/search")
+@auth.login_required
 def get_search():
     try:
         q = request.args["q"]
     except KeyError:
         return []
     else:
+        user = utl.get_login_user()
+        tenant_id = user.get("tenant_id")
+        # user_id = user.get("user_id")
         q_str = "(?i).*" + q + ".*"
-        cql = "MATCH (m) WHERE m.name =~ '{}' or m.entity_code=~ '{}' RETURN m".format(q_str, q_str)
+        cql = "MATCH (m) WHERE (m.name =~ '{}' or m.entity_code=~ '{}')and((m.public_flag is null and m.tenant_id is null) or m.public_flag='Y' or  m.tenant_id={}) RETURN m".format(
+            q_str, q_str, tenant_id)
         results = graph().run(cql)
 
         return Response(dumps([serialize_model(record['m']) for record in results]),
@@ -87,15 +111,26 @@ def get_search():
 
 
 @app.route(domain_root + "/search_shortest_path")
+@auth.login_required
 def search_shortest_path():
     try:
         q = request.args["q"]
         to = request.args["to"]
+        flag = request.args["flag"]
+        sfrom = ""
+        sto = ""
+        # flag==0,则是all，==1则是Child子类关系查询，==2其他则是父类Parent关系查询
+        if flag is not None and flag == "1":
+            sfrom = ""
+            sto = ">"
+        elif flag is not None and flag == "2":
+            sfrom = "<"
+            sto = ""
     except KeyError:
         return []
     else:
-        cql = "MATCH (p1 {name:'%s'}),(p2{name:'%s'}),p=shortestpath((p1)-[*..10]-(p2))RETURN p" % (
-            q, to)
+        cql = "MATCH (p1 {name:'%s'}),(p2{name:'%s'}),p=shortestpath((p1)%s-[*..10]-%s(p2))RETURN p" % (
+            q, to, sfrom, sto)
         result = graph().run(cql)
         nodes, relationships = relationship_mapping(result)
         data = combine_data(nodes, relationships)
@@ -119,21 +154,50 @@ def relationship_mapping(path):
     nodes = []
     relationships = []
     id_list = []
+    user = utl.get_login_user()
+    tenant_id = user.get("tenant_id")
+    # user_id = user.get("user_id")
+    not_privilege_list = []
     if path is not None:
         for res in path:
             for paths in res:
                 logger.info("path:{}".format(paths))
                 for node in paths.nodes:
+                    # 权限判断
+                    bool = have_entity_privilege(tenant_id, node)
+                    if (not bool):
+                        not_privilege_list.append(node.get('entity_id'))
+                        continue
                     d_start, is_exist = node2dict(node, id_list)
                     if not is_exist and d_start is not None:
                         nodes.append(d_start)
                 relationship_list = paths.relationships
                 for relationship in relationship_list:
+                    from_enty_id = relationship['from_entity_id']
+                    to_enty_id = relationship['to_entity_id']
+                    is_true = False
+                    for no_priv_entity_id in not_privilege_list:
+                        if (from_enty_id == no_priv_entity_id or to_enty_id == no_priv_entity_id):
+                            is_true = True
+                            break
+                    if is_true:
+                        continue
                     d_rel = relationship2dict(relationship)
                     if d_rel is not None:
                         relationships.append(d_rel)
         logger.info("relation_info:{},{}".format(nodes, relationships))
     return nodes, relationships
+
+
+def have_entity_privilege(tenant_id, node):
+    have_bool = False
+    ten_id = node.get('tenant_id')
+    flag = node.get('public_flag')
+    if (ten_id is None and flag is None):
+        have_bool = True
+    elif (ten_id == tenant_id or flag == 'Y'):
+        have_bool = True
+    return have_bool
 
 
 def combine_data(nodes, relationships):
@@ -186,8 +250,8 @@ def gen_new_id(old_id, name):
     id = ""
     if id is not None:
         id = str(old_id)
-    if id is not None and len(id) > 3:
-        id = id[len(id) - 3:]
+    if id is not None and len(id) > 2:
+        id = id[len(id) - 2:]
     s = name
     id = s + "(" + id + ")"
     return id
@@ -235,14 +299,22 @@ def query_relation(title, flag):
     elif flag is not None and flag == "2":
         sfrom = "<"
         sto = ""
-    cql = "MATCH (m {name:'%s'}) OPTIONAL MATCH (m)%s-[r]-%s(n) RETURN m.name as name,m.entity_code as title," \
-          "COLLECT([n.name, r.label,r.from_fields_name,r.to_fields_name]) AS cast LIMIT 100" % (title, sfrom, sto)
+    cql = "MATCH (m {name:'%s'}) OPTIONAL MATCH (m)%s-[r]-%s(n) RETURN m.name as name,m.entity_id as entity_id,m.entity_code as title,m.tenant_id as tenant_id,m.public_flag as public_flag," \
+          "COLLECT([r.name, r.label,r.relation_id,r.relation_type,r.relation_desc,r.from_entity_id,r.from_entity_name,r.from_fields_name,r.to_entity_id,r.to_entity_name,r.to_fields_name]) AS cast LIMIT 100" % (
+              title, sfrom, sto)
     result = graph().run(cql)
+    user = utl.get_login_user()
+    tenant_id = user.get("tenant_id")
     re_list = []
     for item in result:
         dict1 = {}
         dict1["title"] = item.get('title')
         dict1["name"] = item.get('name')
+        # ten_id=item.get('tenant_id')
+        # public_flag=item.get("public_flag")
+        bool = have_entity_privilege(tenant_id, item)
+        if (not bool):
+            continue
         cast = item.get('cast')
         if cast is not None:
             li_cast = []
@@ -258,6 +330,7 @@ def query_relation(title, flag):
 
 
 @app.route(domain_root + "/relation/<title>/<flag>")
+@auth.login_required
 def get_relation(title, flag):
     dict1 = query_relation(title, flag)
     return Response(dumps(dict1), mimetype="application/json")
