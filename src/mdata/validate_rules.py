8c9b643d2aee4599b92cef87600bb7c5ac900a15
JoeIOU <78242596+JoeIOU@users.noreply.github.com>
2021-04-19 16:56:02 +0800

    Update validate_rules.py

--- src/mdata/validate_rules.py
+++ src/mdata/validate_rules.py
@@ -39,6 +39,31 @@ rule_sql = """
         AND r.active_flag = 'Y' 
         AND e.md_entity_id = %s
         """
+rule_ui_template = """
+        SELECT
+            rr.rule_ui_rel_id,
+            r.rule_id,
+            rr.ui_template_id,
+            rr.ui_fields_id,
+            r.rule_code,
+            r.rule_category,
+            r.rule_type,
+            r.rule_name,
+            r.rule_script,
+            r.rule_desc,
+            r.rule_desc_en
+        FROM
+            rules r
+        INNER JOIN rules_ui_rel rr ON rr.rule_id = r.rule_id
+        AND rr.active_flag = 'Y'
+        WHERE
+            (
+                r.tenant_id = %s
+                OR r.public_flag = 'Y'
+            )
+        AND r.active_flag = 'Y'
+        AND rr.ui_template_id = %s
+        """
 
 
 def get_rule_code_list(codes):
@@ -51,9 +76,11 @@ def get_rule_code_list(codes):
     return code_list
 
 
-def get_rules(tenant_id, md_entity_id, rule_codes=None, n_rule_codes=None):
+def get_rules(tenant_id, md_entity_id, rule_codes=None, n_rule_codes=None, validateOnly=False):
     sql = rule_sql
     new_rule_codes = None
+    if validateOnly:
+        sql += " AND r.rule_category='Validation'"
     if rule_codes is not None and len(rule_codes) > 0:
         sql += "  AND r.rule_code in %s"
         new_rule_codes = get_rule_code_list(rule_codes)
@@ -75,61 +102,147 @@ def get_rules(tenant_id, md_entity_id, rule_codes=None, n_rule_codes=None):
     return result
 
 
+def get_rules_ui_template(tenant_id, ui_template_id, rule_codes=None):
+    sql = rule_ui_template
+    new_rule_codes = None
+    if rule_codes is not None and len(rule_codes) > 0:
+        sql += "  AND r.rule_code in %s"
+        new_rule_codes = get_rule_code_list(rule_codes)
+        pass
+
+    conn = db_md()
+    cursor = conn.cursor()
+    if new_rule_codes is not None:
+        cursor.execute(sql, args=(tenant_id, ui_template_id, new_rule_codes,))
+    else:
+        cursor.execute(sql, args=(tenant_id, ui_template_id,))
+    result = cursor.fetchall()
+    result = md.data_type_convert(result)
+    logger.info("get_rules,result:{}".format(result))
+    return result
+
+
+def rule_field_mapping(rules, inputs):
+    if rules is not None and inputs is not None:
+        for item in rules:
+            # field_name = item.get("md_fields_name")
+            rule_code = item.get("rule_code")
+            for item1 in inputs:
+                rule_code1 = item1.get("rule_code")
+                f_name = item1.get("field_name")
+                if rule_code is not None and rule_code == rule_code1 and f_name is not None and len(f_name) > 0:
+                    item["md_fields_name"] = f_name
+                    break
+    return rules
+
+
 def validate_rules(rules, data):
     is_pass = True
     pass_list = []
     not_pass_list = []
-    for item in rules:
-        script = item.get("rule_script")
-        # rule_code = item.get("rule_code")
-        field_name = item.get("md_fields_name")
-        d = None
-        if data is not None:
-            if isinstance(data, dict) and field_name is not None:
-                d = data.get(field_name)
-            else:
-                d = data
-        # desc = item.get("rule_desc")
-        rule_category = item.get("rule_category")
-        type = item.get("rule_type")
-        result = False
-        output = None
-        d_str = None
-        if d is None:
-            logger.warning("validate_rules,data is None.")
-        else:
-            if type and type.lower() == 'regex':  # 正则表达式
-                res = None
-                reg = r'{}'.format(script)
-                d_str = str(d)
-                if rule_category == 'Validation':  # 校验规则
-                    res = rule.match(reg, d_str)
+    d_input = None
+    reg = None
+    item = None
+    error_msg = None
+    if data is None:
+        logger.warning("validate_rules,data is None.")
+    try:
+        for item in rules:
+            script = item.get("rule_script")
+            # rule_code = item.get("rule_code")
+            field_name = item.get("md_fields_name")
+            rule_category = item.get("rule_category")
+            type = item.get("rule_type")
+            result = False
+            output = None
+            item['output'] = output
+            item['result'] = result
+            if data is not None and isinstance(data, list):
+                if len(data) > 0:
+                    data = data[0]
                 else:
-                    res = rule.search(reg, d_str)
+                    data = {}
+
+            res = None
+            # 1.正则表达式校验
+            if type is not None and type.lower() == 'regex':
+                d = None
+                if isinstance(data, dict) and field_name is not None:
+                    d = data.get(field_name)
+                    if d is None:
+                        d = ""
+                        logger.warning("validate_rules,data is None by field={}.".format(field_name))
+                else:
+                    d = data
+                reg = r'{}'.format(script)
+                d_input = str(d)
+                item['input'] = d_input
+                if rule_category == 'Validation':  # 校验规则
+                    res = rule.match(reg, d_input)
+                else:
+                    res = rule.search(reg, d_input)
                 if res is not None:
                     result = True
                     output = res.group()
                 else:
                     is_pass = False
                 logger.info("validate_rules,rule=[{}],data:[{}],result:[{}]".format(reg, d, result))
-            elif type and type.lower() == 'script':
+            # 2.规则按公式计算
+            elif type is not None and type.lower() == 'formula':
+                reg = r'{}'.format(script)
+                if field_name is None:
+                    d_input = data
+                    item['input'] = d_input
+                    reg = reg % (d_input)
+                else:
+                    if isinstance(field_name, list):
+                        tuple_field = ()
+                        for key in field_name:
+                            if isinstance(data, dict):
+                                f = data.get(key)
+                                tuple_field += (f,)
+                        d_input = tuple_field
+                        item['input'] = d_input
+                        reg = reg % d_input
+                    else:
+                        d_input = data.get(field_name)
+                        reg = reg % (d_input)
+                item['input'] = d_input
+                res = eval(reg)
+                if res is not None:
+                    result = True
+                    output = res
+
+            # 3.按脚本计算或判断
+            elif type is not None and type.lower() == 'script':
                 data_dict = data
+                d_input = data_dict
+                item['input'] = d_input
                 exec(script, data_dict)
                 result = data_dict.get("result")
+                output = data_dict.get("output")
                 if result is not None:
                     is_pass = False
+            # 4.调用服务计算或判断
             else:
-                # service invoke
                 pass
-        item['input'] = d_str
-        item['output'] = output
-        item['result'] = result
-        if result:
-            pass_list.append(item)
-        else:
-            not_pass_list.append(item)
 
-    return (is_pass, pass_list, not_pass_list)
+            item['input'] = d_input
+            item['output'] = output
+            item['result'] = result
+            if result:
+                pass_list.append(item)
+            else:
+                not_pass_list.append(item)
+
+    except Exception as ex:
+        error_msg = 'validate_rules exception,rule:%s,input:%s,message:%s' % (reg, d_input, ex)
+        not_pass_list.append(item)
+        logger.error(error_msg)
+        is_pass = False
+        # raise ex
+    finally:
+        return (is_pass, pass_list, not_pass_list, error_msg)
 
 
 if __name__ == '__main__':
