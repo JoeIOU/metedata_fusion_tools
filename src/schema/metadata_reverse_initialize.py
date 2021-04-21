01c43465b8f6c10c863cabecb92220062c2077a8
JoeIOU <78242596+JoeIOU@users.noreply.github.com>
2021-04-21 16:08:36 +0800

    Update metadata_reverse_initialize.py

--- src/schema/metadata_reverse_initialize.py
+++ src/schema/metadata_reverse_initialize.py
@@ -1,9 +1,9 @@
 # ######metadata_reverse_initialize.py
 from schema import schema_mysql as mysql, schema_oracle as ora, schema_pg as pg
-from mdata import metadata_initialize as mdi
+from mdata import metadata_initialize as mdi, metadata as md
 from config.config import cfg as config
-from privilege import user_mngt as ur
 from model import erwin_model_graph as emg
+from privilege import data_privilege as dp, role_privilege as rp, user_mngt as ur
 
 logger = config.logger
 
@@ -22,11 +22,13 @@ if __name__ == '__main__':
     #       "3":"从数据表和字段元数据，初始化生成实体和属性元数据",
     #       "4":"mySql/oracle/pg的外键关系转成元数据实体关系",
     #       "5":"erwin的外键关系转成元数据实体关系",
-    #       "6":"元数据对象生成Neo4J图数据库信息"}
+    #       "6":"元数据对象生成Neo4J图数据库信息",
+    #       "7":"权限码单独补充初始化"}
     step = {
         # "2": "从mysql/oracle/pg数据库反向工程，初始化表和字段元数据",
         # "3": "从数据表和字段元数据，初始化生成实体和属性元数据",
-          "4":"mySql/oracle/pg的外键关系转成元数据实体关系",
+        "4": "mySql/oracle/pg的外键关系转成元数据实体关系",
+          # "7":"权限码单独补充初始化"
     }
 
     # ##### 1.初始化表结构从erWin文件(注意文件路径写法d:/downloads/eSpace_File/xxx.erwin)。
@@ -36,9 +38,9 @@ if __name__ == '__main__':
         logger.info("all tables in[{}],re={}".format(schema, re))
 
     # ####1.2.从mysql/oracle/pg数据库反向工程，初始化表和字段元数据
-    tables_list = ['rules','rules_entity_rel']
+    tables_list = ['index_mapping_t', 'index_date_t', 'index_decimal_t','index_text_t','index_int_t']
     if step.get("2") is not None:
-        re=None
+        re = None
         if db_type == 'mysql':
             re = mysql.reverse_tables_columns(user_id, tenant_id, database_name, schema, tables_list)
         elif db_type == 'oracle':
@@ -52,14 +54,15 @@ if __name__ == '__main__':
     # ####2.从数据表和字段元数据，初始化生成实体和属性元数据
     if step.get("3") is not None:
         # entity_list = [{"entity_code": "Contract", "table_name": "data_t"}, {"entity_code": "BoQ", "table_name": "data_t"}]
-        entity_list = [{"entity_code": "rules", "table_name": "rules"},
-                       {"entity_code": "rules_entity_rel", "table_name": "rules_entity_rel"}]
+        entity_list = [{"entity_code": "ui_template", "table_name": "ui_template"},
+                       {"entity_code": "ui_entity_rel", "table_name": "ui_entity_rel"},
+                       {"entity_code": "ui_fields", "table_name": "ui_fields"}]
         re = mdi.initialize_md_entities_from_tables(user_id, tenant_id, entity_list)
         logger.info("all entity import[{}],re={}".format(schema, re))
 
     # ####3.mySql/oracle/pg的外键关系转成元数据实体关系。
     if step.get("4") is not None:
-        re=None
+        re = None
         if db_type == 'mysql':
             re = mysql.reverse_constraint(user_id, tenant_id, schema, tables_list)
         elif db_type == 'oracle':
@@ -82,3 +85,20 @@ if __name__ == '__main__':
         entity_catagory = 'XXX'  # 分类，增加一大类标签。
         # entity_code_list = None 为None，则初始化全部实体。
         re = mdi.ini_entity_model_graph(tenant_id, entity_code_list, entity_catagory, schema)
+
+    if step.get("7") is not None:
+        # 插入到元数据实体表或者数据视图表，就要增加权限码；以及创建默认的实体字段，即填写不为空的字段（包括主键ID）。
+        entity_type = "Entity"  # Entity或者View
+        entity_codes = ['index_date_t','index_decimal_t','index_int_t']
+        res = md.get_md_entities_id_by_code(entity_codes)
+        ids = []
+        if res is not None and len(res) > 0:
+            for item in res:
+                ids.append(item.get("md_entity_id"))
+
+        # 角色权限
+        re = rp.insert_entity_privilege(user_id, tenant_id, entity_type, ids)
+        logger.info("insert_entity_privilege, entity:[{}],re={}".format(entity_codes, re))
+        # 数据范围权限
+        re = dp.insert_data_privilege(user_id, tenant_id, entity_type, ids)
+        logger.info("insert_data_privilege, entity:[{}],re={}".format(entity_codes, re))
