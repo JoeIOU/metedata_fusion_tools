1eac9b53ebe476d659e663222fa930ab7c4d6315
JoeIOU <78242596+JoeIOU@users.noreply.github.com>
2021-05-28 17:29:38 +0800

    Update view_service.py

--- src/service/view_service.py
+++ src/service/view_service.py
@@ -67,3 +67,41 @@ def query_view_list():
         # raise ex
     finally:
         pass
+
+# 视图查询
+@app_view.route(domain_root + '/services/queryView', methods=['POST', 'GET'])
+@auth.login_required
+def query_view():
+    re = None
+    data = None
+    try:
+        data = utl.request_parse(request)
+        view_id = data.get(GLOBAL_VIEW_ID)
+        page_size = data.get('page_size')
+        current_page = data.get('current_page')
+        if view_id is None:
+            logger.warning('query View, param[$_VIEW_ID] should not be None')
+            re = md.exec_output_status(type=utl.SERVICE_METHOD_GET, status=md.DB_EXEC_STATUS_FAIL, rows=0, data=None,
+                                       message='query View,$_VIEW_ID is None')
+            return re
+        if data is not None:
+            for key in data.keys():
+                if key == GLOBAL_VIEW_ID:
+                    data.pop(GLOBAL_VIEW_ID)
+                    break
+        user = utl.get_login_user()
+        tenant_id = user.get("tenant_id")
+        re = utl.sql_execute_method(tenant_id, view_id, utl.SERVICE_METHOD_VIEW, "queryView", data_list=None,
+                                    where_list=[data])
+
+        logger.info('view result:{}'.format(re))
+        return Response(json.dumps(re), mimetype='application/json')
+    except Exception as ex:
+        msg = "query View error.input:{},message:{}".format(data, ex)
+        logger.error(msg)
+        re = md.exec_output_status(type=utl.SERVICE_METHOD_GET, status=md.DB_EXEC_STATUS_FAIL, rows=0, data=None,
+                                   message=msg)
+        return Response(json.dumps(re), mimetype='application/json')
+        # raise ex
+    finally:
+        pass
