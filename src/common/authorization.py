e44bde761702cbb607971e0c038ee61cedb1012b
JoeIOU <78242596+JoeIOU@users.noreply.github.com>
2022-09-01 14:55:01 +0800

    update

--- src/common/authorization.py
+++ src/common/authorization.py
@@ -1,15 +1,18 @@
 # ####authorization.py
 from flask import Flask, g
 from flask_httpauth import HTTPBasicAuth
-from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+# from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 # from itsdangerous import TimedSerializer as Serializer
-from itsdangerous import BadSignature, SignatureExpired
+# from itsdangerous import BadSignature, SignatureExpired
 from flask_cors import CORS
 import re
 from config.config import cfg as config
 from privilege import user_mngt as ur
 from common import cache
 import datetime
+import time
+import base64
+import hmac
 
 logger = config.logger
 
@@ -22,30 +25,71 @@ auth = HTTPBasicAuth()
 
 
 # 生成token, 有效时间为600min
-def generate_auth_token(user_id, expiration=36000):
-    s = Serializer(SECRET_KEY, expires_in=expiration)
-    utc = datetime.datetime.utcnow()
-    timestamp = utc.timestamp()
-    utc_msecond = int(round(timestamp * 1000))
-    expire_time = utc_msecond + (expiration * 1000)
-    re = {'user_id': user_id}
-    logger.info('token:{}'.format(re))
-    return (s.dumps(re), expire_time)
+# def generate_auth_token(user_id, expiration=36000):
+#     s = Serializer(SECRET_KEY, expires_in=expiration)
+#     utc = datetime.datetime.utcnow()
+#     timestamp = utc.timestamp()
+#     utc_msecond = int(round(timestamp * 1000))
+#     expire_time = utc_msecond + (expiration * 1000)
+#     re = {'user_id': user_id}
+#     logger.info('token:{}'.format(re))
+#     return (s.dumps(re), expire_time)
+
+
+def generate_auth_token(key, expire=3600):
+    r'''
+    @Args:
+     key: str (用户给定的key，需要用户保存以便之后验证token,每次产生token时的key 都可以是同一个key)
+     expire: int(最大有效时间，单位为s)
+    @Return:
+     state: str
+    '''
+    ts_str = str(time.time() + expire)
+    ts_byte = ts_str.encode("utf-8")
+    sha1_tshexstr = hmac.new(key.encode("utf-8"), ts_byte, 'sha1').hexdigest()
+    token = ts_str + ':' + sha1_tshexstr
+    b64_token = base64.urlsafe_b64encode(token.encode("utf-8"))
+    return (b64_token.decode("utf-8"), expire)
 
 
 # 解析token
+# def verify_auth_token(token):
+#     s = Serializer(SECRET_KEY)
+#     # token正确
+#     try:
+#         data = s.loads(token)
+#         return data
+#     # token过期
+#     except SignatureExpired:
+#         return None
+#     # token错误
+#     except BadSignature:
+#         return None
+
 def verify_auth_token(token):
-    s = Serializer(SECRET_KEY)
-    # token正确
-    try:
-        data = s.loads(token)
-        return data
-    # token过期
-    except SignatureExpired:
+    r'''
+    @Args:
+     key: str
+     token: str
+    @Returns:
+     boolean
+    '''
+    token_str = base64.urlsafe_b64decode(token).decode('utf-8')
+    token_list = token_str.split(':')
+    if len(token_list) != 2:
         return None
-    # token错误
-    except BadSignature:
+    ts_str = token_list[0]
+    if float(ts_str) < time.time():
+        # token expired
         return None
+    known_sha1_tsstr = token_list[1]
+    # sha1 = hmac.new(key.encode("utf-8"),ts_str.encode('utf-8'),'sha1')
+    # calc_sha1_tsstr = sha1.hexdigest()
+    # if calc_sha1_tsstr != known_sha1_tsstr:
+    # token certification failed
+    #  return False
+    # token certification success
+    return known_sha1_tsstr
 
 
 def login_verify(user_account, password):
@@ -97,4 +141,7 @@ def verify_password(username, password, force=False):
 
 if __name__ == '__main__':
     # logger.info("tokenDecode:{}".format('s'))
-    generate_auth_token('test1', 36000)
+    (s, ex) = generate_auth_token('test1', 36000)
+    logger.info("token:{}".format(s))
+    ss = verify_auth_token(s)
+    logger.info("tokenDecode:{}".format(ss))
